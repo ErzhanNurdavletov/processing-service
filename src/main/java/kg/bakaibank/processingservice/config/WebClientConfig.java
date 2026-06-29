3bf789c660decd74ddfeca7bd583e32834faa3cd
Эржан Нурдавлетов <nurdavletoverjan@gmail.com>
2026-06-29 14:44:17 +0600

    refactor: externalize WebClient base URL and simplify datasource configuration

--- src/main/java/kg/bakaibank/processingservice/config/WebClientConfig.java
+++ src/main/java/kg/bakaibank/processingservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package kg.bakaibank.processingservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("limits.service.api")
+    private String limitServiceApi;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-            .baseUrl("http://localhost:8080/api/v1")
+            .baseUrl(limitServiceApi)
             .build();
     }
 }
