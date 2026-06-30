67f7ce4ade44f21ad8061ad0358dd240c0eddfaf
Эржан Нурдавлетов <nurdavletoverjan@gmail.com>
2026-06-30 16:15:11 +0600

    add @Testcontainers

--- src/test/java/kg/bakaibank/processingservice/ProcessingServiceApplicationTests.java
+++ src/test/java/kg/bakaibank/processingservice/ProcessingServiceApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 @SpringBootTest
 class ProcessingServiceApplicationTests {
 
