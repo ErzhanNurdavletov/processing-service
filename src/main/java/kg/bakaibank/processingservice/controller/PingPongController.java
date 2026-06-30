121c48ba7f97684e40476a775edc8e4da7de9148
Эржан Нурдавлетов <nurdavletoverjan@gmail.com>
2026-06-30 16:47:00 +0600

    add ping endpoint 2

--- src/main/java/kg/bakaibank/processingservice/controller/PingPongController.java
+++ src/main/java/kg/bakaibank/processingservice/controller/PingPongController.java
@@ -1,5 +1,7 @@
 package kg.bakaibank.processingservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class PingPongController {
     @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    public ResponseEntity<?> ping() {
+        return new ResponseEntity<>("pong", HttpStatus.OK);
     }
 }
